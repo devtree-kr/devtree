1d52d3f38a2110dae83a95e7d729210d9a3518c6
Yoonsoo Kim <laptise@live.jp>
2022-08-14 01:52:35 +0900

    add test method

--- app/post/views.py
+++ app/post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import PostSerailzer
 from .models import Post
 
@@ -10,3 +11,7 @@ from .models import Post
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerailzer
+
+    @action(methods=['get'], detail=False)
+    def testGet(self, request):
+        return Response("Its post Test!")
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import UserSerailzer
 from .models import User
 # Create your views here.
@@ -9,3 +10,7 @@ from .models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerailzer
+
+    @action(methods=['get'], detail=False)
+    def testGet(self, request):
+        return Response("Its user Test!")
