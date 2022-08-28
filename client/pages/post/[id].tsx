e024146be29d98d6da381630add9b16d779c3f5b
Yoonsoo Kim <laptise@live.jp>
2022-08-28 18:32:26 +0900

    post view page

--- client/pages/post/[id].tsx
+++ client/pages/post/[id].tsx
@@ -1,14 +1,40 @@
 import { User } from "@entities";
 import { GetServerSideProps, NextPage } from "next";
-import { ssrAxios } from "../../axios";
+import { ssrAxios, isBrowser } from "../../axios";
 import Layout from "../../components/layout";
 import { withAuth } from "../../ssr/auth";
 import { SinglePostView } from "@dtos";
+import { Box } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+//@ts-ignore
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const SinglePost: NextPage<{ auth: User; postView: SinglePostView }> = ({ auth, postView }) => {
   return (
     <Layout title={postView.title} auth={auth}>
-      HI!
+      <Box className="markdown-content" style={{ flex: 1 }}>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match && isBrowser() ? (
+                <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div" {...props}>
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {postView.content}
+        </ReactMarkdown>
+      </Box>
     </Layout>
   );
 };
