63251bce24e60aac3bccaea4c5edf5407607353e
Yoonsoo Kim <laptise@live.jp>
2022-08-28 19:12:51 +0900

    게시물 페이지

--- client/pages/post/[id].tsx
+++ client/pages/post/[id].tsx
@@ -4,36 +4,50 @@ import { ssrAxios, isBrowser } from "../../axios";
 import Layout from "../../components/layout";
 import { withAuth } from "../../ssr/auth";
 import { SinglePostView } from "@dtos";
-import { Box } from "@mui/material";
+import { Box, Chip, Divider, Stack, Tooltip } from "@mui/material";
 import ReactMarkdown from "react-markdown";
 //@ts-ignore
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-
+import { Typography } from "@mui/material";
+import FaceIcon from "@mui/icons-material/Face";
 const SinglePost: NextPage<{ auth: User; postView: SinglePostView }> = ({ auth, postView }) => {
   return (
     <Layout title={postView.title} auth={auth}>
-      <Box className="markdown-content" style={{ flex: 1 }}>
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match && isBrowser() ? (
-                <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div" {...props}>
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
-        >
-          {postView.content}
-        </ReactMarkdown>
+      <Box className="markdown-content" style={{ flex: 1, backgroundColor: "white", padding: 40, minWidth: "80%" }}>
+        <Stack spacing={2}>
+          <Typography variant="h1" sx={{ fontSize: "3rem" }}>
+            {postView.title}
+          </Typography>
+          <Stack direction="row" spacing={1}>
+            {postView.tags.map((tag) => (
+              <Tooltip key={tag.id} placement="top" title={tag.tagNmKr}>
+                <Chip icon={<FaceIcon />} label={tag.tagNmEn} variant="outlined" />
+              </Tooltip>
+            ))}
+          </Stack>
+          <Divider />
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div" {...props}>
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          >
+            {postView.content}
+          </ReactMarkdown>
+        </Stack>
       </Box>
     </Layout>
   );
